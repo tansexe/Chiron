e03705edc1162f1ec8444164d7f6e6995009e140
Taniya Nawal Pathak <112875959+tansexe@users.noreply.github.com>
2025-04-09 19:27:35 +0530

    commit

--- app.py
+++ app.py
@@ -115,4 +115,5 @@ def query():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=True)
